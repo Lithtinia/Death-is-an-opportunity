3363e47e7dd83aa16a4a41e491d353a9c80a193b
Lithtinia <darkninja65786@gmail.com>
2025-10-04 12:21:25 +1000

    soul counter and tracking is they are correct is done off to lunch

---
+++ Assets/scripts/Manager stuff/choice made.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class choicemade : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public soulcarstuff SCS;
+    public GameMananger GM;
+
+   public void OnTriggerEnter(Collider other)
+   {
+       SCS = other.gameObject.GetComponent<soulcarstuff>();
+       GM.PplInLine = GM.PplInLine - 1;
+       
+       if (SCS.good == true)
+       {
+           GM.CorrectC = GM.CorrectC+ 1;
+           
+       }
+   }
+}
---
+++ Assets/scripts/Manager stuff/soulcar stuff.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class soulcarstuff : MonoBehaviour
+{
+    public bool good;
+    public bool bad;
+
+    private GameMananger gameman;
+   
+
+    // Update is called once per frame
+    public void fixedUpdate()
+    {
+        
+    }
+}
